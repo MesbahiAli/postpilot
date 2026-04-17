0c26766049676111850fc40ba19b5a20e9cff1d6
alimesbahi <mali@adsolutions.ma>
2026-04-17 23:38:35 +0100

    feat: add Dev.to scraper as second content source

--- scrapers/reddit_scraper.py
+++ scrapers/reddit_scraper.py
@@ -6,9 +6,21 @@ keywordsList = [
     "Deep Learning", "FastAPI", "Data Science", "GenAI",
     "Generative AI", "AI Agents", "Neural Network",
     "Computer Vision", "NLP", "Robotics", "Blockchain",
-    "Quantum Computing", "AI Safety", "AI Ethics"
+    "Quantum Computing", "AI Safety", "AI Ethics","MCP"
 ]
-def get_trending_posts(limit=20):
+
+
+def filter_posts(postsFromHack):
+    posts = []
+    for post in postsFromHack:
+        for keyword in keywordsList:
+            if re.search(rf'\b{re.escape(keyword)}\b', post['title'], re.IGNORECASE):
+                posts.append(post)
+                break
+    return posts
+               
+
+def get_trending_posts_hackNews(limit=20):
     top_stories_url = "https://hacker-news.firebaseio.com/v0/topstories.json"
     
     response = requests.get(top_stories_url)
@@ -25,20 +37,27 @@ def get_trending_posts(limit=20):
             "source": "HackerNews"
         })
     
-    return posts
+    return filter_posts(posts)
+
+
+def get_trending_posts_DEVto(limit=3):
+    top_stories_url = "https://dev.to/api/articles?top=1"
+    
+    response = requests.get(top_stories_url)
+    story = response.json()[:limit]
+    posts = []
+    for article in story:
+        posts.append({
+                "title": article.get("title", ""),
+                "score": article.get("public_reactions_count", 0),
+                "url": article.get("url", ""),
+                "source": "DevTo"
+            })
+    
+    return filter_posts(posts)
 
     
-postsFromHack=get_trending_posts(limit=20)
 
 
-def filter_posts(postsFromHack):
-    posts = []
-    for post in postsFromHack:
-        for keyword in keywordsList:
-            if re.search(rf'\b{re.escape(keyword)}\b', post['title'], re.IGNORECASE):
-                posts.append(post)
-                break
-    return posts
-               
 
-postFinal=filter_posts(postsFromHack)
+postFinal=get_trending_posts_hackNews(limit=20)+get_trending_posts_DEVto(limit=20)
